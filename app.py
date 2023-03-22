fffca967fe8b80fcacb069593978bad4872574d9
FirdiansyAH <mazrizky46@gmail.com>
2023-03-22 17:00:53 +0700

    first commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-db = client(DB_NAME)
+db = client[DB_NAME]
 app = Flask(__name__)
 
 @app.route('/')
